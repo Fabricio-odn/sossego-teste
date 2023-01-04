c8c51d09fabc445ce2063e092f79b5a5d7d08756
Fabricio Oliveira <fabriciodeoliveira334@gmail.com>
2023-01-03 21:14:37 -0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Icon, Spacer } from '@chakra-ui/react';
+import { RiUser5Fill, RiHome4Fill, RiFileTextFill } from 'react-icons/ri';
+
+import {
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Box,
+  Center,
+  Circle,
+  Text,
+} from '@chakra-ui/react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Flex align="center" justify="center">
+        <Container
+          bg="#fff"
+          borderRadius="8px"
+          border="2px solid #F0F2F6"
+          boxShadow="0px
+          4px 31px #EEF1F5"
+          w="1017px"
+          p="3rem"
+          marginTop={50}
         >
-          Learn React
-        </a>
-      </header>
+          <Text fontSize="2.25rem" fontWeight="bold">
+            Criação de Usuário
+          </Text>
+          <Flex align="center" gap={20} wrap="wrap">
+            <Circle size="50px" bg="#F0F2F6" color="#5357B1">
+              <Icon as={RiUser5Fill} w={20} h={20} />
+            </Circle>
+            <Text fontWeight="500">Identificação Usuário</Text>
+
+            <Circle size="50px" bg="#F0F2F6" color="#8C98A9">
+              <Icon as={RiHome4Fill} w={20} h={20} />
+            </Circle>
+            <Text>Endereço do Usuário</Text>
+
+            <Circle size="50px" bg="#F0F2F6" color="#8C98A9">
+              <Icon as={RiFileTextFill} w={20} h={20} />
+            </Circle>
+            <Text>Sobre Você</Text>
+          </Flex>
+        </Container>
+      </Flex>
     </div>
   );
 }
